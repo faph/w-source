876270c96a31dc66e43599a956f51a70d49f6117
Florenz Hollebrandse <faph@users.noreply.github.com>
2016-08-13 20:02:44 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
 set -e
 
-# Deploy built docs to this branch
-TARGET_BRANCH=master
-REPO="git@github.com:peterychuang/peterychuang.github.io.git"
-
 if [ ! -d "$SOURCE_DIR" ]; then
   echo "SOURCE_DIR ($SOURCE_DIR) does not exist, build the source directory before deploying"
   exit 1
 fi
 
 if [ -n "$TRAVIS_BUILD_ID" ]; then
-  # When running on Travis we need to use SSH to deploy to GitHub
-  #
-  # The following converts the repo URL to an SSH location,
-  # decrypts the SSH key and sets up the Git config with
-  # the correct user name and email (globally as this is a
-  # temporary travis environment)
-  #
-  # Set the following environment variables in the travis configuration (.travis.yml)
-  #
-  #   DEPLOY_BRANCH    - The only branch that Travis should deploy from
-  #   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
-  #   GIT_NAME         - The Git user name
-  #   GIT_EMAIL        - The Git user email
-  #
-  echo DEPLOY_BRANCH: $DEPLOY_BRANCH
+  echo FROM_BRANCH: $FROM_BRANCH
   echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
   echo GIT_NAME: $GIT_NAME
-  echo GIT_EMAIL: $GIT_EMAIL
-  if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
-    echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
+  if [ "$TRAVIS_BRANCH" != "$FROM_BRANCH" ]; then
+    echo "Travis should only deploy from the FROM_BRANCH ($FROM_BRANCH) branch"
     exit 0
   else
     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
@@ -41,18 +22,18 @@ if [ -n "$TRAVIS_BUILD_ID" ]; then
       eval `ssh-agent -s`
       ssh-add $SSH_KEY
       git config --global user.name "$GIT_NAME"
-      git config --global user.email "$GIT_EMAIL"
+      git config --global user.email "$GIT_NAME@users.noreply.github.com"
     fi
   fi
 fi
 
-REPO_NAME=$(basename $REPO)
-TARGET_DIR=$(mktemp -d /tmp/$REPO_NAME.XXXX)
+TO_REPO_NAME=$(basename $TO_REPO)
+TO_DIR=$(mktemp -d /tmp/$TO_REPO_NAME.XXXX)
 REV=$(git rev-parse HEAD)
-git clone --branch ${TARGET_BRANCH} ${REPO} ${TARGET_DIR}
-rsync -rt --delete --exclude=".git" --exclude=".travis.yml" $SOURCE_DIR/ $TARGET_DIR/
-cd $TARGET_DIR
+git clone --branch ${TO_BRANCH} ${TO_REPO} ${TO_DIR}
+rsync -rt --delete --exclude=".git" --exclude=".travis.yml" $SOURCE_DIR/ $TO_DIR/
+cd $TO_DIR
 ls -all
 #git add -A .
 #git commit --allow-empty -m "Built from commit $REV"
-#git push $REPO $TARGET_BRANCH
+#git push $TO_REPO $TO_BRANCH
